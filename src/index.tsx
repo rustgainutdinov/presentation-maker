6c81ae9a8a2d4a73da6bbc2d8a72747835270693
rustgainutdinov <rustgainutdinov@gmail.com>
2020-12-21 22:40:35 +0300

    переделал компоненты на tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,13 @@ import {createStore} from 'redux';
 import './index.css';
 import App from './App';
 import {rootReducer} from "./store/Reducer";
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(rootReducer, composeWithDevTools());
 ReactDOM.render(
 	<Provider store={store}>
 		<App/>
 	</Provider>,
 	document.getElementById('root')
 );
-
